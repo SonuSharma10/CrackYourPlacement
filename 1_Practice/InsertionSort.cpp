6ac0106e26f75f58288039d1c60fd87c1388ce05
SonuSharma10 <sonu.g.sharma10@gmail.com>
2024-12-11 00:39:55 +0530

    Selection Sort

---
+++ 1_Practice/InsertionSort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void print(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int InsertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int current = i;
+        for (int j = i - 1; j >= 0; j--)
+        {
+
+            if (arr[j] > arr[current])
+            {
+                swap(arr[j], arr[current]);
+                current = j;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {5, 7, 9, 4, 2, 1, 0};
+    int n = sizeof(arr) / sizeof(int);
+    InsertionSort(arr, n);
+    print(arr, n);
+}
---
+++ 1_Practice/SelectionSort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void print(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int SelectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIdx = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIdx])
+            {
+                minIdx = j;
+            }
+        }
+        swap(arr[i], arr[minIdx]);
+    }
+}
+
+int main()
+{
+    int arr[] = {5, 4, 3, 2, 1};
+    int n = sizeof(arr) / sizeof(int);
+    SelectionSort(arr, n);
+    print(arr, n);
+}
