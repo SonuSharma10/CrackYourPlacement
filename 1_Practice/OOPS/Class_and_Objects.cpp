9e218f9c97d06c456033986b795abf5b8dc411a1
SonuSharma10 <sonu.g.sharma10@gmail.com>
2025-01-07 16:51:55 +0530

    OOPS Concept CLass And Objects

--- 1_Practice/OOPS/Class_and_Objects.cpp
+++ 1_Practice/OOPS/Class_and_Objects.cpp
@@ -2,6 +2,13 @@
 #include <string>
 using namespace std;
 
+// OOPS: Object-Oriented Programming System
+
+// Class: A class is a user-defined data type that can have data members and member functions. The data members are the attributes of the class, and the member functions are the methods of the class. The class is defined using the keyword class followed by the class name. The attributes and methods of the class are defined
+// inside the class using the access specifiers public, private, or protected. The class definition is terminated by a semicolon. For example, class Student { }; defines a class Student.
+// class is not a function, rather it becoes a data type
+
+// Object : An object is an instance of a class. When a class is defined, no memory is allocated for the data members of the class. Memory is allocated only when an object of the class is created. An object is created using the class name followed by the object name and the semicolon. For example, Student s1; creates an object s1 of the class Student.
 class Student
 {
 private:
