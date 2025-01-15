93fd241f2e2118327d6caeb656c41574ba15906d
SonuSharma10 <sonu.g.sharma10@gmail.com>
2025-01-16 00:43:19 +0530

    Minimize XOR

---
+++ Leatcode/BitManipulation/Minimize XOR.cpp
@@ -0,0 +1,82 @@
+// Given two positive integers num1 and num2, find the positive integer x such that:
+
+// x has the same number of set bits as num2, and
+// The value x XOR num1 is minimal.
+// Note that XOR is the bitwise XOR operation.
+
+// Return the integer x. The test cases are generated such that x is uniquely determined.
+
+// The number of set bits of an integer is the number of 1's in its binary representation.
+
+// Example 1:
+
+// Input: num1 = 3, num2 = 5
+// Output: 3
+// Explanation:
+// The binary representations of num1 and num2 are 0011 and 0101, respectively.
+// The integer 3 has the same number of set bits as num2, and the value 3 XOR 3 = 0 is minimal.
+// Example 2:
+
+// Input: num1 = 1, num2 = 12
+// Output: 3
+// Explanation:
+// The binary representations of num1 and num2 are 0001 and 1100, respectively.
+// The integer 3 has the same number of set bits as num2, and the value 3 XOR 1 = 2 is minimal.
+
+// Constraints:
+
+// 1 <= num1, num2 <= 10^9
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+private:
+    bool notsetbit(int x, int count)
+    {
+        if (x & 1 << count)
+            return false;
+        else
+            return true;
+    }
+    void setbit(int &x, int count)
+    {
+        x = x | 1 << count;
+    }
+    void unsetbit(int &x, int count)
+    {
+        x = x & ~(1 << count);
+    }
+
+public:
+    int minimizeXor(int num1, int num2)
+    {
+        int result = num1;
+        int b1 = __builtin_popcount(num1);
+        int b2 = __builtin_popcount(num2);
+        int position = 0;
+
+        while (b2 > b1)
+        {
+            if (notsetbit(result, position))
+            {
+                setbit(result, position);
+                b1++;
+            }
+            position++;
+        }
+
+        while (b2 < b1)
+        {
+            if (!notsetbit(result, position))
+            {
+                unsetbit(result, position);
+                b2++;
+            }
+            position++;
+        }
+
+        return result;
+    }
+};
