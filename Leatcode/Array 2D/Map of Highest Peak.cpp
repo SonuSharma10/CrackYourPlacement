ff4adba0904029e868b36e80c7376d5dcd6b53fb
SonuSharma10 <sonu.g.sharma10@gmail.com>
2025-02-02 18:42:45 +0530

    Map of Highest Peak

--- Leatcode/Array 2D/Map of Highest Peak.cpp
+++ Leatcode/Array 2D/Map of Highest Peak.cpp
@@ -31,3 +31,77 @@
 // 1 <= m, n <= 1000
 // isWater[i][j] is 0 or 1.
 // There is at least one water cell.
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>> &isWater)
+    {
+        int row = isWater.size();
+        int col = isWater[0].size();
+        vector<vector<int>> ans(row, vector<int>(col, row * col));
+        // top and Left pass
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                // Fix the water Cell to 0
+                if (isWater[i][j])
+                {
+                    ans[i][j] = 0;
+                }
+                // top and left pass
+                else
+                {
+                    if (i > 0)
+                    {
+                        ans[i][j] = min(ans[i][j], ans[i - 1][j] + 1);
+                    }
+                    if (j > 0)
+                    {
+                        ans[i][j] = min(ans[i][j], ans[i][j - 1] + 1);
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                cout << ans[i][j] << " ";
+            }
+            cout << endl;
+        }
+
+        // bottom and right pass
+        for (int i = row - 1; i >= 0; i--)
+        {
+            for (int j = col - 1; j >= 0; j--)
+            {
+                // bottom and right pass
+                if (i < row - 1)
+                {
+                    ans[i][j] = min(ans[i][j], ans[i + 1][j] + 1);
+                }
+                if (j < col - 1)
+                {
+                    ans[i][j] = min(ans[i][j], ans[i][j + 1] + 1);
+                }
+            }
+        }
+
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                cout << ans[i][j] << " ";
+            }
+            cout << endl;
+        }
+
+        return ans;
+    }
+};
