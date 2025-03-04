89c193e51fb72f6d64125ba730ad36ea26cb73d3
SonuSharma10 <sonu.g.sharma10@gmail.com>
2025-03-04 23:57:00 +0530

    XOR Operation in an Array

--- Leatcode/BitManipulation/XOR Operation in an Array.cpp
+++ Leatcode/BitManipulation/XOR Operation in an Array.cpp
@@ -34,7 +34,6 @@ public:
         for (int i = 1; i < n; i++)
         {
             ans = ans ^ (start + 2 * i);
-            // cout<<ans<<"   "<<((start + 2) * i)<<"   ";
         }
         return ans;
     }
